e03b03051710a39c3e00b67769b0367b47036b66
Jesus-RA <jesus.ra98@hotmail.com>
2021-10-21 02:01:32 -0400

    Added endpoint to fetch user role and permissions and added api version prefix v1

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,25 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Order\OrderController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Profile\ProfileController;
+use App\Http\Controllers\Auth\RoleWithAbilitiesController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('login', [LoginController::class, 'login'])->name('login');
+Route::post('register', [RegisterController::class, 'register'])->name('register');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function(){
+
+    Route::get('orders', [OrderController::class, 'index'])->name('orders.index');
+    Route::get('orders/{order}', [OrderController::class, 'show'])->name('orders.show');
+    Route::post('orders', [OrderController::class, 'store'])->name('orders.store');
+    Route::match(['put', 'patch'], 'orders/{order}', [OrderController::class, 'update'])->name('orders.update');
+
+    Route::get('profiles/{profile}', [ProfileController::class, 'show'])->name('profiles.show');
+    Route::match(['put', 'patch'], 'profiles/{profile}', [ProfileController::class, 'update'])->name('profiles.update');
+
+    Route::get('role-abilities', [RoleWithAbilitiesController::class, 'index'])->name('role_abilities');
+
 });
