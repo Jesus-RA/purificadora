435b7b387b6433fceaa4345c86d5388febea9d83
Jesus-RA <jesus.ra98@hotmail.com>
2021-10-21 02:05:35 -0400

    Added sanctum to authenticate users

---
+++ database/migrations/2021_10_08_134852_create_addresses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('calle');
+            $table->string('numero_exterior');
+            $table->string('numero_interior')->nullable();
+            $table->string('colonia');
+            $table->string('municipio');
+            $table->string('estado');
+            $table->string('codigo_postal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
