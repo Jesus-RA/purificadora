c8c8052185a21239a904e323a4e4bf481f1e954c
Jesus-RA <jesus.ra98@hotmail.com>
2021-10-21 02:02:21 -0400

    Seeding data in DatabaseSeeder.php file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use App\Models\Address\Address;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +18,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $admin = Role::create(['name' => 'admin']);
+        $client = Role::create(['name' => 'client']);
+
+        $create_order = Permission::create(['name' => 'create orders']);
+        $view_order = Permission::create(['name' => 'view its order']);
+        $update_orders = Permission::create(['name' => 'update orders']);
+
+        $admin->syncPermissions([
+            Permission::create(['name' => 'view all orders']),
+            $create_order,
+            $view_order,
+            $update_orders
+        ]);
+
+        $client->syncPermissions([
+            $create_order,
+            $view_order,
+            $update_orders,
+            Permission::create(['name' => 'view its profile']),
+            Permission::create(['name' => 'update its profile']),
+        ]);
+
+        User::create([
+            'email' => 'jesus@gmail.com',
+            'password' => Hash::make('jamon')
+        ])->assignRole( $admin );
+        
         $this->call(AddressSeeder::class);
         \App\Models\User::factory(10)->create();
         $this->call(ProfileSeeder::class);
