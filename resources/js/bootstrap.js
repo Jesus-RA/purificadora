0274e00de64de1d6ef9091ef6c8ce208ad53bdba
Jesus-RA <jesus.ra98@hotmail.com>
2021-11-23 23:45:33 -0500

    Added withCredentials option to axios object

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,6 +24,7 @@ window.axios.defaults.baseURL = document.head.querySelector('meta[name="api-base
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.headers.common['Accept'] = 'application/json';
+window.axios.defaults.withCredentials = true
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
